d5345b66f937e2916d9ebc6c9c12bf73afd9f193
HONGYAN <zhy19930227@gmail.com>
2019-08-26 21:37:30 +1000

    created the login, account, register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountComponent,
+    RegisterComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        {
+         path: 'appcomponent',
+         component: AppComponent
+        },
+
+        { path: 'account', 
+         component: AccountComponent 
+        },
+        {
+         path: 'login',
+         component: LoginComponent
+        },
+        {
+          path: 'register',
+          component: RegisterComponent
+        }    
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
